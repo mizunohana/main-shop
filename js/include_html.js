b5ad8429562d9897905065669e25a74b97ac4972
Tomoyuki Yamada <yamada.flobal@gmail.com>
2020-07-15 16:48:49 +0900

    include系整備

---
+++ js/include_html.js
@@ -0,0 +1,5 @@
+//ヘッダー
+ $(function(){ $.ajaxSetup({cache:false}); $("#include_header").load("./include/header.html"); });
+
+ //showroom
+ $(function(){ $.ajaxSetup({cache:false}); $("#include_showroom").load("./include/showroom.html"); });
