036bd400cf612118da87651005c01d6ee08b0afd
Tomoyuki Yamada <yamada.flobal@gmail.com>
2020-07-15 16:48:30 +0900

    showroom

--- header.js
+++
@@ -1,5 +0,0 @@
-//ヘッダー
- $(function(){ $.ajaxSetup({cache:false}); $("#include").load("header.html"); });
-
- //showroom
- $(function(){ $.ajaxSetup({cache:false}); $("#include_showroom").load("showroom.html"); });
